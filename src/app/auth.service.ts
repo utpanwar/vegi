6af0f61769fdca1755d5ef43aa87fc02e03f7abd
Utkarsh Panwar <utkarshpanwar212@gmail.com>
2020-05-14 12:40:31 +0530

    first commit

---
+++ src/app/auth.service.ts
@@ -0,0 +1,40 @@
+import { UserService } from './user.service';
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, } from 'rxjs/operators';
+import { AppUser } from './models/app-user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  user$ : Observable<firebase.User>;
+  constructor(
+              private afAuth : AngularFireAuth , 
+              private route : ActivatedRoute,
+              private userService : UserService
+              ){
+                 this.user$=afAuth.authState;
+               }
+   login()
+   {
+      let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+      localStorage.setItem('returnUrl' , returnUrl) ;
+      this.afAuth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+   }
+
+   logout()
+   {
+    this.afAuth.signOut();
+   }
+
+  //  get appUser$() : Observable<AppUser> {
+  //   return this.user$
+  //   .pipe(switchMap(user => this.userService.get(user.uid).valueChanges()));
+  //  }
+
+   
+}
