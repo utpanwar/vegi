1647fa93c78581d40859f6c8e86c0bf26616642c
Utkarsh Panwar <utkarshpanwar212@gmail.com>
2020-05-19 11:15:39 +0530

    CU_RD read sowing list objects

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { ProductService } from './../../product.service';
+import { ProductsComponent } from './../../products/products.component';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
-  styleUrls: ['./admin-products.component.css']
+  styleUrls: ['./admin-products.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AdminProductsComponent implements OnInit {
-
-  constructor() { }
+  products$;
+  constructor(private productService :ProductService) {
+    this.products$ = this.productService.getAll();
+    // console.log(this.products$);
+    // this.productService.getAll().subscribe(re=>console.log(re));
+    //this method use to see the object in console
+   }
 
   ngOnInit() {
   }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,16 @@ export class ProductService {
     
     return this.db.list('/product').push(product);
   }
-
+  getAll() {
+    return this.db.list('/product')
+      .snapshotChanges()
+      .pipe(map(changes => changes.map(c => ({
+        $key: c.payload.key, $value: c.payload.val()
+      }))));
+  }
+  // getAll(product)
+  // {
+  //   return this.db.list('/product').valueChanges();
+  // }
+  // this is not work it works in angular 4
 }
