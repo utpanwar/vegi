0a5f06671379a9399345490545cd8c115d68f504
Utkarsh Panwar <utkarshpanwar212@gmail.com>
2020-05-26 14:32:47 +0530

    i understand valueChanges SnapshotChanges upto  and edit button works fine upto this

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -21,7 +21,9 @@ export class ProductService {
       .pipe(map(changes => changes.map(c => ({
         $key: c.payload.key, $value: c.payload.val()
       }))));
-  }
+  } 
+  //actually snapshot changes are rreturn obseervable<snapshot> but we changes this into
+  //  observable of $key ,$value pair with the help of map operator
   // getAll(product)
   // {
   //   return this.db.list('/product').valueChanges();
@@ -29,9 +31,14 @@ export class ProductService {
   // this is not work it works in angular 4 beacuse valuechanges()  return the only value in the 
   // form of json array type Observable
   // but Snapshot returns metadata also like key of firebase .it returns key value pair  
+  // get(productId)
+  // {
+  //    return this.db.object('/product/' + productId).snapshotChanges();
+    // this also works fine 
+  // }
   get(productId)
   {
-     return this.db.object('/product/' + productId).snapshotChanges();
+     return this.db.object('/product/' + productId).valueChanges();
   }
    update(productID,product)
    {
