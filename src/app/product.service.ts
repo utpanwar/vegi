7af70f0dd316941099570472dd90d39dc5405f74
Utkarsh Panwar <utkarshpanwar212@gmail.com>
2020-05-31 01:57:43 +0530

    it trats path as akey  and gives key payload type value as a object

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -24,18 +24,18 @@ export class ProductService {
   // } 
   getAll() {
     let ref= this.db.list('/product').snapshotChanges();
-    let ref1= this.db.list('/product').snapshotChanges();
+    let ref1= this.db.object('/product').snapshotChanges();
     // let ref2= this.db.list('/product').snapshotChanges();
-    // ref1.subscribe(x =>{ console.log(x); console.log("go")});
+    ref1.subscribe(x =>{ console.log(x); console.log("go")});
     // ref2.subscribe(x =>{ console.log(x); console.log("go2")});
       //  ref1.pipe(map(c => ({
       // $key: c.payload.key, $value: c.payload.val()
       // // console.log("ji");
       // }))).subscribe(x => console.log(x));
       
-      ref1.pipe(map(changes => changes.map(c => ({   
-        $key: c.payload.key, $value: c.payload.val()
-      })))).subscribe(x => console.log(x));
+      // ref1.pipe(map(changes => changes.map(c => ({   
+      //   $key: c.payload.key, $value: c.payload.val()
+      // })))).subscribe(x => console.log(x));
     // like in .sub we have payload and key in each object inside the Array
     // now extract exact data with set theese two property
      return  ref.pipe(map(changes => changes.map(c => ({
