7101c9323353abfc7759f34093a12ac18c97aa3e
Utkarsh Panwar <utkarshpanwar212@gmail.com>
2020-06-07 17:23:49 +0530

    this file is of prevoius branch pull this

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,6 @@
 import { ShoppingCart } from './../models/shopping-cart';
+
+                  //  it takes component for the category and the bootstrap cart
 import { ShoppingCartService } from './../shopping-cart.service';
 import { switchMap } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
@@ -14,12 +16,17 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent   {
+
+  // product$: any;
   products=[];
   filteredProducts=[];
+  // categories$ : any;
   category: any;
   cart : ShoppingCart;
   subscribe :Subscription;
-
+  // while constructor is part of ES6 JavaScript class
+  // const should be used for only DI, it is creater first while the component tree is created all the const are
+  // initialized
   constructor(private productService : ProductService, 
               private categoryService : CategoryService,
               private route :ActivatedRoute ,
@@ -39,12 +46,40 @@ export class ProductsComponent   {
         this.products.filter(p => p.$value.category === this.category) :
         this.products
       });
-    }
+      
+     
+    // here we have two asyn call so we dont know which one is executed first so shows blank page on 
+    // startup product array is empty we solve this with the switchMap operator
 
 
+   }
+  //  OK, first of all ngOnInit is part of Angular lifecycle hook it is called after the componet tree and 
+  // onChange() hook
+  //ngOninit call after the component tree is created or it is called after default changedetectection() hook
+//   ngOnChanges is called when an input or output binding value changes
+// ngOnInit is called after the first ngOnChanges
+// Component interpolated template and input initial values aren't 
+// available in constructor, but they are available in ngOnInit
+
+
+// Asynchronous initialization
+
+// Asynchronous initialization constructor can often be considered 
+// antipattern and have smell because class instantiation finishes before asynchronous 
+// routine does, and this can create race conditions. If it's not the case, 
+// ngOnInit and other lifecycle hooks are better places for this, particularly
+//  because they can benefit from async syntax: so here we use asyn call in ngOnit
+
+// flow is here
+// Constructor() --> ngOnChanges() --> ngOnInit()5432w1q  `
    async ngOnInit(){
+     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+     //Add 'implements OnInit' to the class.
      console.log("Triggered ONINIT by product.component.ts");
      this.subscribe= (await this.shoppingCartService.getCart()).subscribe(cart => {this.cart = cart;
+      // The ngOnInit() hooks only once after all directives are instantiated.
+      //  If you have subscription insidengOnInit() 
+      //  and it's not unsubscribed then it will run again if the subscribed data changes
       console.log(cart)
       console.log
       ("this cart data comes from product OnInit in last because they subscribe it")});
@@ -52,6 +87,7 @@ export class ProductsComponent   {
       console.log("end ONINIT of product.component.ts");
     
    }
+  //  getCart return object of shoppingCart class so we deorate this in to shoppingCart
    ngOnDestroy(){
     this.subscribe.unsubscribe();
   }
