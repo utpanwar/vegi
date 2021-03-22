8eb21c3c9078ce4b83bb1f6ede8692cd1bb5ecb2
utpanwar <utpanwar75@gmail.com>
2021-03-22 11:40:03 +0530

    ngForms works

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,14 +10,20 @@ export class LoginComponent implements OnDestroy {
   constructor(private auth : AuthService) {
    }
 
-log(x)
-{
-  console.log(x);
-}
- login()
- {
-    this.auth.login();
- }
+  log(x)
+  {
+    console.log(x);
+  }
+
+  Submit(f)
+  {
+    console.log(f); // it prints the complete object of ngForm
+    console.log(f.value);//it print the only vale in that form in key:value
+  }
+  login()
+  {
+      this.auth.login();
+  }
  ngOnDestroy()
  {
    console.log("%c i login.com.ts destroy", "color:red; font-size:13px");
